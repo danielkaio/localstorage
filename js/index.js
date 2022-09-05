8ef9c47085caabb0ae5fc42fa7166230b64b8c25
daniel <dani.ajala@yahoo.com>
2022-09-05 18:42:58 -0300

    mostrando itens local storage  na  tela

--- js/index.js
+++ js/index.js
@@ -2,22 +2,28 @@ let form = document.querySelector('form')
 let task  = JSON.parse(localStorage.getItem('task'))|| []
 let list = document.querySelector('.list')
 
+task.forEach(task => {
+  
+    let linha = document.createElement("li")
+    linha.innerHTML = task
+    
+    list.append(linha)
+
+});
 
 form.addEventListener("submit",(e)=>{
 
     let nome = document.querySelector('#nome').value
-
     task.push(nome)
-
-    localStorage.setItem("task",JSON.stringify(task))
-   
-
-    let linha = document.createElement("li")
     
+   function Insert (){
+    localStorage.setItem("task",JSON.stringify(task))
     linha.innerHTML = nome
     list.appendChild(linha)
+   }
 
-
+   Insert()
+   
     e.preventDefault()
 
 
