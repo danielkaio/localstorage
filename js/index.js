c05f9dab6d39ad7edea78e8f08e6bc4a107fb779
daniel <dani.ajala@yahoo.com>
2022-09-05 02:06:56 -0300

    localstorage

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let form = document.querySelector('form')
+let task  = JSON.parse(localStorage.getItem('task'))|| []
+let list = document.querySelector('.list')
+
+
+form.addEventListener("submit",(e)=>{
+
+    let nome = document.querySelector('#nome').value
+
+    task.push(nome)
+
+    localStorage.setItem("task",JSON.stringify(task))
+
+    let linha = document.createElement("li")
+    linha.innerHTML = nome
+    list.appendChild(linha)
+
+
+    e.preventDefault()
+
+
+
+
+    
+    
+})
+
