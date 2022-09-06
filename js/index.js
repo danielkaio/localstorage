4e40fc7915d9abc394f96865d034b3f7f506d75a
daniel <dani.ajala@yahoo.com>
2022-09-06 00:32:36 -0300

    pequeno-ajuste

--- js/index.js
+++ js/index.js
@@ -6,30 +6,38 @@ task.forEach(task => {
   
     let linha = document.createElement("li")
     linha.innerHTML = task
-    
     list.append(linha)
-
+    let btn = document.createElement('button')
+    btn.innerText = "x"
+    linha.append(btn)
+    btn.addEventListener('click',function(){
+    
+        linha.remove(this.parentNode)
+        
+    })
+    
 });
 
 form.addEventListener("submit",(e)=>{
 
     let nome = document.querySelector('#nome').value
+ 
     task.push(nome)
-    
+
    function Insert (){
     localStorage.setItem("task",JSON.stringify(task))
     linha.innerHTML = nome
     list.appendChild(linha)
+
    }
 
    Insert()
+
+
+   
    
     e.preventDefault()
 
-
-
-
-    
     
 })
 
